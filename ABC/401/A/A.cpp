0c1be5c49e91f2b19c87ae3d2af667e72f73df47
towaos <towagura3@gmail.com>
2025-11-13 17:23:05 +0900

    update

---
+++ ABC/401/A/A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int S;
+  cin >> S;
+
+  if(200<=S&&S< 300){
+    cout << "Success" << endl;
+    return 0;
+  }else{
+    cout << "Failure" << endl;
+    return 0;
+  }
+}
